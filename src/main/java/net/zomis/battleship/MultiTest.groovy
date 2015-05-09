a6d42c4c89fb65b40c57de961c6c6c613309f799
Simon Forsberg <zomis2k@hotmail.com>
2015-05-09 18:54:15 +0200

    added MultiTest, to more easily test multiplayer

---
+++ src/main/java/net/zomis/battleship/MultiTest.groovy
@@ -0,0 +1,31 @@
+package net.zomis.battleship
+
+
+def scanner = new Scanner(System.in);
+
+print "Enter name: "
+def nameA = scanner.nextLine();
+def clientA = new Client(name: nameA)
+new Thread({ clientA.listen() }).start();
+
+print "Enter name: "
+def nameB = scanner.nextLine();
+def clientB = new Client(name: nameB)
+new Thread({ clientB.listen() }).start();
+
+Thread.sleep 200
+clientA.send("INVT Battleship $nameB")
+Thread.sleep 200
+clientB.send("INVY 0")
+Thread.sleep 200
+clientA.send("MOVE 0 SHIP Air_Carrier 5 1 0 0 Battleship 4 1 0 1 Submarine 3 1 0 2 Submarine 3 1 0 3 Patrol 2 1 0 4")
+Thread.sleep 200
+clientB.send("MOVE 0 SHIP Air_Carrier 5 1 0 0 Battleship 4 1 0 1 Submarine 3 1 0 2 Submarine 3 1 0 3 Patrol 2 1 0 4")
+Thread.sleep 200
+
+clientA.send("MOVE 0 PLAY 0 0")
+clientA.send("MOVE 0 PLAY 0 1")
+clientA.send("MOVE 0 PLAY 0 2")
+clientA.send("MOVE 0 PLAY 0 3")
+clientA.send("MOVE 0 PLAY 0 4")
+clientA.send("MOVE 0 PLAY 0 5")
