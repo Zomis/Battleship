7140f5525697cf39c66023caf35e73af88671854
Simon Forsberg <zomis2k@hotmail.com>
2015-05-09 18:53:53 +0200

    refactored main

--- src/main/java/net/zomis/battleship/Main.groovy
+++ src/main/java/net/zomis/battleship/Main.groovy
@@ -8,47 +8,45 @@ public class Client {
 	
 	def socket = new Socket("localhost", 7282);
 	def writer = new PrintWriter(socket.outputStream);
-
+	def name
+	
 	public Client() {
-		new Thread({ console() }).start();
-	}
-	
-	def console() {
-		def scan = new Scanner(System.in);
-		
-		print "Enter your name: "
-		def name = scan.nextLine()
-		new Thread({ listen(name) }).start();
-		
-		scan.withCloseable({ scanner ->
-			while (true) {
-				def input = scanner.nextLine();
-				send input
-//				println input
-			}
-		});
 		
 	}
 	
-	def listen(name) {
-		
+	def listen() {
 		send "USER xxx $name password"
-		
-		
 		def input = socket.inputStream;
 		def reader = new BufferedReader(new InputStreamReader(input));
 		
-		reader.lines().forEach({mess -> System.out.println mess})
+		reader.lines().forEach({mess -> System.out.println name + " Incoming: " + mess})
 	}
 	
 	def send(message) {
-		System.out.println "Send " + message
+		System.out.println name + " Send: " + message
 		writer.print(message + (char) 0)
 		writer.flush()
 	}
 }
 
-def client = new Client()
+def scan = new Scanner(System.in);
+print "Enter your name: "
+def name = scan.nextLine()
 
+def client = new Client(name: name)
 
+def console(Client client, Scanner scan) {
+	
+	new Thread({ client.listen() }).start();
+	
+	scan.withCloseable({ scanner ->
+		while (true) {
+			def input = scanner.nextLine();
+			client.send input
+//				println input
+		}
+	});
+	
+}
 
+new Thread({ console(client) }).start();
